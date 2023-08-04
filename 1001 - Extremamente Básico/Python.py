bc5d6079a597848a75fe8aef8aec996cd1dfddee
Arthur Nunes <arthurvininunes@gmail.com>
2023-08-04 00:21:16 -0300

    Solution - 04/08/2023

---
+++ 1001 - Extremamente Básico/Python.py
@@ -0,0 +1,4 @@
+a = int(input())
+b = int(input())
+print("X =", {a + b})
+# Por um motivo que não entendo, o BeeCrowd não quis aceitar saída como print(f"X= {a+b}").
---
+++ JavaScript.js
@@ -0,0 +1,7 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var a = parseInt(lines[0]);
+var b = parseInt(lines[1]);
+
+console.log("X = " + (a + b));
